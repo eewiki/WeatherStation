6ded30228bdbc96e051c53c785af4fb297a2b3dd
Robert Nelson <robertcnelson@gmail.com>
2017-02-03 10:02:28 -0600

    need to fix 2nd ina219 sensor
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ws_bm_rtc.ino
+++ ws_bm_rtc.ino
@@ -7,7 +7,7 @@
 
 RTC_DS1307 rtc;
 Adafruit_INA219 ina219_A;
-Adafruit_INA219 ina219_B(0x41);
+//Adafruit_INA219 ina219_B(0x41);
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -18,8 +18,8 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 
 /* 8Mhz */
 #define ws_baud 115200
-#define ws_one_second 2000
-#define ws_short_delay 400
+#define ws_one_second 500
+#define ws_short_delay 100
 
 void setup () {
 	clock_prescale_set(clock_div_2);
@@ -45,7 +45,7 @@ void setup () {
 	// By default the initialization will use the largest range (32V, 2A).  However
 	// you can call a setCalibration function to change this range (see comments).
 	ina219_A.begin();
-	ina219_B.begin();
+//	ina219_B.begin();
 	// To use a slightly lower 32V, 1A range (higher precision on amps):
 	//ina219.setCalibration_32V_1A();
 	// Or to use a lower 16V, 400mA range (higher precision on volts and amps):
@@ -87,10 +87,12 @@ void loop () {
 	current_mA_A = ina219_A.getCurrent_mA();
 	loadvoltage_A = busvoltage_A + (shuntvoltage_A / 1000);
 
+/*
 	shuntvoltage_B = ina219_B.getShuntVoltage_mV();
 	busvoltage_B = ina219_B.getBusVoltage_V();
 	current_mA_B = ina219_B.getCurrent_mA();
 	loadvoltage_B = busvoltage_B + (shuntvoltage_B / 1000);
+*/
 
 	delay(ws_short_delay);
 
@@ -114,6 +116,10 @@ void loop () {
 
 	delay(ws_short_delay);
 
+/*
+
+	delay(ws_short_delay);
+
 	Serial1.print("$SOLAR:"); Serial1.print(now.unixtime());
 	Serial1.print(":BusVolt:"); Serial1.print(busvoltage_B); Serial1.println("V*");
 
@@ -131,9 +137,10 @@ void loop () {
 
 	Serial1.print("$SOLAR:"); Serial1.print(now.unixtime());
 	Serial1.print(":Current:"); Serial1.print(current_mA_B); Serial1.println("mA*");
+*/
+
 	Serial1.println("");
 
-	delay(ws_short_delay);
 	delay(ws_short_delay);
 	delay(ws_one_second);
 	delay(ws_one_second);
