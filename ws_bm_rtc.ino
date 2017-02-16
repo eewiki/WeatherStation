456c0bb1b86949093def589b003c28c379c2b3c2
Robert Nelson <robertcnelson@gmail.com>
2017-02-16 10:16:31 -0600

    switched to Arduino Pro 328p, 3.3v, 8mhz
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ws_bm_rtc.ino
+++ ws_bm_rtc.ino
@@ -2,38 +2,32 @@
 #include <avr/power.h>
 #include "Adafruit_INA219.h"
 #include "RTClib.h"
+#include "LowPower.h"
 
 RTC_DS1307 rtc;
 Adafruit_INA219 ina219_A;
 Adafruit_INA219 ina219_B(0x44);
 
-/* 16Mhz */
+/* 8Mhz (fuse/bootloader) */
 #define ws_baud 57600
 #define ws_one_second 1000
 #define ws_short_delay 200
 
-/* 8Mhz */
-//#define ws_baud 115200
-//#define ws_one_second 500
-//#define ws_short_delay 100
-
 const int XBee_wake = 9;
 
-int sensorPin = 0;
-
 void setup () {
-	//clock_prescale_set(clock_div_2);
+	//clock_prescale_set(clock_div_4);
 
-	Serial1.begin(ws_baud);
-	Serial1.println("Hello!");
+	Serial.begin(ws_baud);
+	Serial.println("Hello!");
 
 	if (! rtc.begin()) {
-		Serial1.println("Couldn't find RTC");
+		Serial.println("Couldn't find RTC");
 		while (1);
 	}
 
 	if (! rtc.isrunning()) {
-		Serial1.println("RTC is NOT running!");
+		Serial.println("RTC is NOT running!");
 		// following line sets the RTC to the date & time this sketch was compiled
 		// rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 		// This line sets the RTC with an explicit date & time, for example to set
@@ -51,7 +45,7 @@ void setup () {
 	// Or to use a lower 16V, 400mA range (higher precision on volts and amps):
 	//ina219.setCalibration_16V_400mA();
 
-	Serial1.println("Measuring voltage and current with INA219 ...");
+	Serial.println("Measuring voltage and current with INA219 ...");
 }
 
 void loop () {
@@ -75,100 +69,76 @@ void loop () {
 	busvoltage_B = ina219_B.getBusVoltage_V();
 	current_mA_B = ina219_B.getCurrent_mA();
 
-	int reading = analogRead(sensorPin);
-	float voltage = reading * 5.0;
-	voltage /= 1024.0;
-	float temperatureC = (voltage - 0.5) * 100;
-	float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
-
 	// wake up the XBee
 	pinMode(XBee_wake, OUTPUT);
 	digitalWrite(XBee_wake, LOW);
 	delay(ws_short_delay);
 
-	Serial1.print("$TEMP:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":RAW:");
-	Serial1.print(voltage);
-	Serial1.print(":CALC:");
-	Serial1.print(temperatureF); Serial1.println("F*");
+	Serial.print("$DATE:");
+	Serial.print(now.year(), DEC);
+	Serial.print('/');
+	Serial.print(now.month(), DEC);
+	Serial.print('/');
+	Serial.print(now.day(), DEC);
+	Serial.print("_");
+	Serial.print(now.hour(), DEC);
+	Serial.print(':');
+	Serial.print(now.minute(), DEC);
+	Serial.print(':');
+	Serial.print(now.second(), DEC);
+	Serial.print('*');
+	Serial.println();
+
+	Serial.print("$LOAD:");
+	Serial.print(now.unixtime());
+	Serial.print(":BusVolt:");
+	Serial.print(busvoltage_A);
+	Serial.println("V*");
 	delay(ws_short_delay);
 
-	Serial1.print("$DATE:");
-	Serial1.print(now.year(), DEC);
-	Serial1.print('/');
-	Serial1.print(now.month(), DEC);
-	Serial1.print('/');
-	Serial1.print(now.day(), DEC);
-	Serial1.print("_");
-	Serial1.print(now.hour(), DEC);
-	Serial1.print(':');
-	Serial1.print(now.minute(), DEC);
-	Serial1.print(':');
-	Serial1.print(now.second(), DEC);
-	Serial1.print('*');
-	Serial1.println();
-
-	Serial1.print("$LOAD:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":BusVolt:");
-	Serial1.print(busvoltage_A);
-	Serial1.println("V*");
-	delay(ws_short_delay);
-
-	Serial1.print("$LOAD:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":Current:");
-	Serial1.print(current_mA_A);
-	Serial1.println("mA*");
+	Serial.print("$LOAD:");
+	Serial.print(now.unixtime());
+	Serial.print(":Current:");
+	Serial.print(current_mA_A);
+	Serial.println("mA*");
 	delay(ws_short_delay);
 
 	shuntvoltage_A = ina219_A.getShuntVoltage_mV();
 	busvoltage_A = ina219_A.getBusVoltage_V();
 	current_mA_A = ina219_A.getCurrent_mA();
 
-	Serial1.print("$CHARGER:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":BusVolt:");
-	Serial1.print(busvoltage_B);
-	Serial1.println("V*");
+	Serial.print("$CHARGER:");
+	Serial.print(now.unixtime());
+	Serial.print(":BusVolt:");
+	Serial.print(busvoltage_B);
+	Serial.println("V*");
 	delay(ws_short_delay);
 
-	Serial1.print("$CHARGER:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":Current:");
-	Serial1.print(current_mA_B);
-	Serial1.println("mA*");
-
-	Serial1.print("$LOADXB:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":BusVolt:");
-	Serial1.print(busvoltage_A);
-	Serial1.println("V*");
+	Serial.print("$CHARGER:");
+	Serial.print(now.unixtime());
+	Serial.print(":Current:");
+	Serial.print(current_mA_B);
+	Serial.println("mA*");
 	delay(ws_short_delay);
 
-	Serial1.print("$LOADXB:");
-	Serial1.print(now.unixtime());
-	Serial1.print(":Current:");
-	Serial1.print(current_mA_A);
-	Serial1.println("mA*");
+	Serial.print("$LOADXB:");
+	Serial.print(now.unixtime());
+	Serial.print(":BusVolt:");
+	Serial.print(busvoltage_A);
+	Serial.println("V*");
 	delay(ws_short_delay);
 
-	Serial1.println("");
+	Serial.print("$LOADXB:");
+	Serial.print(now.unixtime());
+	Serial.print(":Current:");
+	Serial.print(current_mA_A);
+	Serial.println("mA*");
+	Serial.println("");
 	delay(ws_short_delay);
 
 	// put the XBee to sleep
 	pinMode(XBee_wake, INPUT); // put pin in a high impedence state
 	digitalWrite(XBee_wake, HIGH);
 
-	delay(ws_one_second);
-	delay(ws_one_second);
-	delay(ws_one_second);
-	delay(ws_one_second);
-
-	delay(ws_one_second);
-	delay(ws_one_second);
-	delay(ws_one_second);
-	delay(ws_one_second);
-	delay(ws_one_second);
+	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
