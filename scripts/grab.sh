6308b293826b2861c722a577bcecbfb3082fb26e
Robert Nelson <robertcnelson@gmail.com>
2017-03-10 09:16:39 -0600

    more debugging
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/grab.sh
+++ scripts/grab.sh
@@ -12,7 +12,7 @@ do
 	load_busvolt=$(echo $READ | sed 's/ /\n/g' | grep '$LOAD:' | grep BusVolt | awk -F ':' '{print $4}' | awk -F 'V*' '{print $1}' || true)
 	echo "load_busvolt=[$load_busvolt]"
 	load_current=$(echo $READ | sed 's/ /\n/g' | grep '$LOAD:' | grep Current | awk -F ':' '{print $4}' | awk -F 'mA*' '{print $1}' || true)
-	echo "load_current[=$load_current]"
+	echo "load_current=[$load_current]"
 
 	charger_busvolt=$(echo $READ | sed 's/ /\n/g' | grep '$CHARGER:' | grep BusVolt | awk -F ':' '{print $4}' | awk -F 'V*' '{print $1}' || true)
 	echo "charger_busvolt=[$charger_busvolt]"
@@ -25,11 +25,13 @@ do
 		echo "$get_time,$load_busvolt" >> /var/www/html/dygraphs/load_voltage_data.csv
 		echo "$get_time,$load_current" >> /var/www/html/dygraphs/load_current_data.csv
 		echo "$get_time,$load_busvolt,$charger_busvolt" >> /var/www/html/dygraphs/voltage_data.csv
+		echo "save:[$get_time,$load_busvolt,$charger_busvolt]"
 	fi
 
 	if [ "x$charger_busvolt" != "x" ] && [ "x$charger_current" != "x" ] ; then
 		echo "$get_time,$charger_busvolt" >> /var/www/html/dygraphs/charger_voltage_data.csv
 		echo "$get_time,$charger_current" >> /var/www/html/dygraphs/charger_current_data.csv
 		echo "$get_time,$load_current,$charger_current" >> /var/www/html/dygraphs/current_data.csv
+		echo "save:[$get_time,$load_current,$charger_current]"
 	fi
 done
