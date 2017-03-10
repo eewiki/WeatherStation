2710e9afdc4b6a3361410eee866ac229f1305b7c
Robert Nelson <robertcnelson@gmail.com>
2017-03-10 13:44:17 -0600

    weather box
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- www/install.sh
+++ www/install.sh
@@ -5,4 +5,5 @@ git pull --rebase
 cp -v dygraph.css /var/www/html/dygraphs/
 cp -v dygraph.min.js /var/www/html/dygraphs/
 cp -v power.html /var/www/html/dygraphs/
+cp -v weather_box.html /var/www/html/dygraphs/
 
