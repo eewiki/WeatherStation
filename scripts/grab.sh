6df09f6bb7a8ad03905921906f4bcdde0ed75fab
Robert Nelson <robertcnelson@gmail.com>
2017-03-10 09:13:53 -0600

    more debugging
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/grab.sh
+++ scripts/grab.sh
@@ -8,11 +8,16 @@ stty -F ${port} raw speed 57600 &> /dev/null
 while [ 1 ];
 do
 	READ=` grabserial -d /dev/ttyUSB0 -b 57600 -m "$" -q "^\n"`
+	echo "[$READ]"
 	load_busvolt=$(echo $READ | sed 's/ /\n/g' | grep '$LOAD:' | grep BusVolt | awk -F ':' '{print $4}' | awk -F 'V*' '{print $1}' || true)
+	echo "load_busvolt=[$load_busvolt]"
 	load_current=$(echo $READ | sed 's/ /\n/g' | grep '$LOAD:' | grep Current | awk -F ':' '{print $4}' | awk -F 'mA*' '{print $1}' || true)
+	echo "load_current[=$load_current]"
 
 	charger_busvolt=$(echo $READ | sed 's/ /\n/g' | grep '$CHARGER:' | grep BusVolt | awk -F ':' '{print $4}' | awk -F 'V*' '{print $1}' || true)
+	echo "charger_busvolt=[$charger_busvolt]"
 	charger_current=$(echo $READ | sed 's/ /\n/g' | grep '$CHARGER:' | grep Current | awk -F ':' '{print $4}' | awk -F 'mA*' '{print $1}' || true)
+	echo "charger_current=[$charger_current]"
 
 	get_time=$(env TZ=America/North_Dakota/Center date +"%Y/%m/%d %k:%M:%S")
 
