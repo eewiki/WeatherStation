04018d4432585ff8be7afa2da712e4da7bfe3635
Robert Nelson <robertcnelson@gmail.com>
2017-03-10 10:05:15 -0600

    rename
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- www/install.sh
+++ www/install.sh
@@ -4,5 +4,5 @@ git pull --rebase
 
 cp -v dygraph.css /var/www/html/dygraphs/
 cp -v dygraph.min.js /var/www/html/dygraphs/
-cp -v index.html /var/www/html/dygraphs/
+cp -v power.html /var/www/html/dygraphs/
 
