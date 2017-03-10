38d83dde2d475da18bbaf1d7df2010ccdd7c7251
Robert Nelson <robertcnelson@gmail.com>
2017-03-10 13:21:15 -0600

    add this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/grab.sh
+++ scripts/grab.sh
@@ -19,6 +19,18 @@ do
 	charger_current=$(echo $READ | sed 's/ /\n/g' | grep '$CHARGER:' | grep Current | tail -1 | awk -F ':' '{print $4}' | awk -F 'mA*' '{print $1}' || true)
 	echo "charger_current=[$charger_current]"
 
+#$WBH:23:Humid:22.43P*
+#$WBP:23:Pres:99554.00Pa*
+#$WBT:23:TempF:14.11F*
+
+	wbh=$(echo $READ | sed 's/ /\n/g' | grep '$WBH:' | grep Humid | tail -1 | awk -F ':' '{print $4}' | awk -F 'P*' '{print $1}' || true)
+	echo "wbh=[$wbh]"
+	wbp=$(echo $READ | sed 's/ /\n/g' | grep '$WBP:' | grep Pres | tail -1 | awk -F ':' '{print $4}' | awk -F 'Pa*' '{print $1}' || true)
+	echo "wbp=[$wbp]"
+	wbt=$(echo $READ | sed 's/ /\n/g' | grep '$WBT:' | grep TempF | tail -1 | awk -F ':' '{print $4}' | awk -F 'F*' '{print $1}' || true)
+	echo "wbt=[$wbt]"
+
+
 	get_time=$(env TZ=America/North_Dakota/Center date +"%Y/%m/%d %k:%M:%S")
 
 	if [ "x$load_busvolt" != "x" ] && [ "x$load_current" != "x" ] ; then
@@ -37,5 +49,17 @@ do
 			echo "$get_time,$load_current,$charger_current" >> /var/www/html/dygraphs/current_data.csv
 			echo "save:[$get_time,$load_current,$charger_current]"
 		fi
+	fi
+
+	if [ "x$wbh" != "x" ] ; then
+		echo "$get_time,$wbh" >> /var/www/html/dygraphs/wbh_data.csv
+	fi
+
+	if [ "x$wbp" != "x" ] ; then
+		echo "$get_time,$wbp" >> /var/www/html/dygraphs/wbp_data.csv
+	fi
+
+	if [ "x$wbt" != "x" ] ; then
+		echo "$get_time,$wbt" >> /var/www/html/dygraphs/wbt_data.csv
 	fi
 done
