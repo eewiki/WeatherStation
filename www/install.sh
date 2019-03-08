f9a32cb6d8dbda2bbd796afe942fc9ca3e2f20e5
Robert Nelson <robertcnelson@gmail.com>
2019-03-08 11:22:02 -0600

    add SAML weather
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- www/install.sh
+++ www/install.sh
@@ -6,4 +6,4 @@ cp -v dygraph.css /var/www/html/dygraphs/
 cp -v dygraph.min.js /var/www/html/dygraphs/
 cp -v power.html /var/www/html/dygraphs/
 cp -v weather_box.html /var/www/html/dygraphs/
-
+cp -v SAML11_weather.html /var/www/html/dygraphs/
