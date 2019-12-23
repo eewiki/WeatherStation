0d9fbe667080f350a50042c35401eee1f3d220d7
Robert Nelson <robertcnelson@gmail.com>
2019-12-23 17:18:21 -0600

    add one for all
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- www/install.sh
+++ www/install.sh
@@ -8,3 +8,4 @@ cp -v power.html /var/www/html/dygraphs/
 cp -v weather_box.html /var/www/html/dygraphs/
 cp -v SAML11_weather.html /var/www/html/dygraphs/
 cp -v Solar.html /var/www/html/dygraphs/
+cp -v DFRobot.html /var/www/html/dygraphs/
