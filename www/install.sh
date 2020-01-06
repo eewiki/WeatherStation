4b65c322a58732ce016e6de3fc9829f7ff466133
Robert Nelson <robertcnelson@gmail.com>
2020-01-06 13:30:26 -0600

    add sensor
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- www/install.sh
+++ www/install.sh
@@ -5,6 +5,7 @@ git pull --rebase
 cp -v dygraph.css /var/www/html/dygraphs/
 cp -v dygraph.min.js /var/www/html/dygraphs/
 cp -v power.html /var/www/html/dygraphs/
+cp -v pth_sensor.html /var/www/html/dygraphs/
 cp -v weather_box.html /var/www/html/dygraphs/
 cp -v SAML11_weather.html /var/www/html/dygraphs/
 cp -v Solar.html /var/www/html/dygraphs/
